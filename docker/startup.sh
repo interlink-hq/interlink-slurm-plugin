dfc7eb670962bc87431a2a8347e292de5cf9b07f
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2025-08-20 11:23:22 +0200

    update base plugin image
    Signed-off-by: Diego Ciangottini <diego.ciangottini@pg.infn.it>

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,8 +3,8 @@
 # Determine whether script is running as root
 sudo_cmd=""
 if [ "$(id -u)" != "0" ]; then
-    sudo_cmd="sudo"
-    sudo -k
+  sudo_cmd="sudo"
+  sudo -k
 fi
 
 # Configure Slurm to use maximum available processors and memory
@@ -13,12 +13,12 @@ ${sudo_cmd} bash <<SCRIPT
 sed -i "s/<<HOSTNAME>>/$(hostname)/" /etc/slurm/slurm.conf
 sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm/slurm.conf
 sed -i "s/<<MEMORY>>/$(if [[ "$(slurmd -C)" =~ RealMemory=([0-9]+) ]]; then echo "${BASH_REMATCH[1]}"; else exit 100; fi)/" /etc/slurm/slurm.conf
-service munge start
-service slurmd start
+service munge start 
+service slurmd start 
 service slurmctld start
 SCRIPT
 
 # Revoke sudo permissions
 if [[ ${sudo_cmd} ]]; then
-    sudo -k
+  sudo -k
 fi
